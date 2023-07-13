a16c223152cd2a377ac044c3779a404dee06c52e
IshitaShaneshah <ishaneshah@argusoft.com>
2023-07-13 17:04:02 +0530

    [""Working Ishita Branch"]

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Register from './Components/Register';
-import  Login from './Components/Login';
+// import logo from './logo.svg';
+// import './App.css';
+// import Register from './Components/Register';
+// import  Login from './Components/Login';
 
 
-function App() {
-  return (
-  <>
-  <Register/>
-  {/* <Login/> */}
-  </>
-  );
-}
+// function App() {
+//   return (
+//   <>
+//   <Register/>
+//   {/* <Login/> */}
+//   </>
+//   );
+// }
 
-export default App;
+// export default App;
 import './App.css';
 // import Home from './Components/Home';
 // import PostProduct from './Components/PostProduct';
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{useState} from 'react'
 import "../CSS/Register.css"
 const Login = () => {
@@ -22,33 +21,19 @@ const Login = () => {
     }
 
   }
-=======
-import React from 'react'
-import "../CSS/Register.css"
-const Login = () => {
->>>>>>> origin/main
   return (
     <div className='register-background'>
     <div className='register'>
         <div className='register-title'>Login</div>
-<<<<<<< HEAD
      <form onSubmit={submitHandler}>
         <div className='input-fields'>
         <label for="email">Email :</label>
         <br/>
         <input id="email" type="email" value={user.email} name="email" onChange={onChangeHandler} required/>
-=======
-     <form>
-        <div className='input-fields'>
-        <label for="email">Email :</label>
-        <br/>
-        <input id="email" type="text"/>
->>>>>>> origin/main
         </div>
         <div className='input-fields'>
         <label for="password">Password :</label>
         <br/>
-<<<<<<< HEAD
         <input id="password" type="password" name="password" value={user.password} onChange={onChangeHandler} required/>
         </div>
         <div className='register-btn'>
@@ -56,15 +41,6 @@ const Login = () => {
         </div>
      </form>
      <div className='login-reg'>Don't have an account? <a href="/">Register</a></div>
-=======
-        <input id="password" type="password"/>
-        </div>
-        <div className='register-btn'>
-        <button type="button" class="btn btn-light">Login</button>
-        </div>
-     </form>
-     <div className='login-reg'>Don't have an account? <a href="#">Register</a></div>
->>>>>>> origin/main
     </div>
     </div>
   )
