332513f22ff9e17124e0ea4abe791581dd281699
ShreyaSha01 <shreyas@argusoft.com>
2023-07-13 15:26:49 +0530

    models and routes

---
+++ src/backend/routes/signuproute.js
@@ -0,0 +1,16 @@
+const signupRoute = require("express").Router();
+const signmodel = require("../models/signupmodel");
+
+signupRoute.post("/signup", async (req, res) =>{
+    try {
+        const data = req.body;
+        const newUser = new signmodel(data);
+        await newUser.save().then(()=>{
+            res.status(200).json({message: "successfull"})
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = signupRoute;
