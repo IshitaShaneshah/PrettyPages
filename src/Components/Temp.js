37984de7a3a90ff872ac8faba805df7820feaa24
IshitaShaneshah <ishaneshah@argusoft.com>
2023-07-15 17:53:13 +0530

    bookcoverdone

--- src/Components/Temp.js
+++ src/Components/Temp.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import "../CSS/Temp.css"
+import openbook from "../photos/openbook.jpg"
 const Temp = () => {
+  
   return (
-    <div>Temp</div>
+    <div>
+      <div className='book'>
+        <div className='page'>
+            <div >
+              <img src={openbook}/>
+            </div>
+        </div>
+         <div className='page-2'>
+            <div className='page2-text'>
+              
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
