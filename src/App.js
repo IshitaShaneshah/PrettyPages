824faf46b06e37213d404033253927b168ce428d
Shreya Sharma <shreyas@argusoft.com>
2023-07-17 15:46:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import  Login from './Components/Login';
 import WishList from './Components/WishList';
 import Cart from './Components/Cart';
 import Temp from './Components/Temp';
+<<<<<<< Updated upstream
 import PostProduct from './Components/PostProduct';
+=======
+import { Axios } from 'axios';
+>>>>>>> Stashed changes
 function App() {
   return (
     <BrowserRouter>
