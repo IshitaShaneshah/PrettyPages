50be250a998daff62733ed8b338e03c7f2825be5
IshitaShaneshah <ishaneshah@argusoft.com>
2023-07-18 12:10:50 +0530

    bookInfo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,13 +5,15 @@ import "../CSS/Home.css"
 import Catalog from './Catalog'
 import Intro from "../Components/Intro"
 import BookDetail from './BookDetail'
+import Temp from './Temp'
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Intro/>
     {/* <Catalog/> */}
-    <BookDetail/>
+    {/* <BookDetail/> */}
+    <Temp/>
     </>
   )
 }
