8a6ef53b96ca8458703ede365e08a29df39a384d
IshitaShaneshah <ishaneshah@argusoft.com>
2023-07-22 18:00:17 +0530

    a

--- src/Components/BookDetail.js
+++ src/Components/BookDetail.js
@@ -13,48 +13,99 @@ const BookDetail = () => {
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac ultricies velit, eu vehicula orci. Vestibulum gravida dapibus elementum. Suspendisse pulvinar convallis nisl, nec sollicitudin eros feugiat imperdiet. Vestibulum at malesuada massa, vel pellentesque dolor. Cras malesuada ex quam, non fermentum elit ornare in.",
   };
   return (
-    // <div className="book-detail container-fluid">
-    //   <div className="book-container row">
-    //     {/* <div className="book-coverbg"> */}
-    //     <div className="book-cover col-md-6">
-    //       <img className="book-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGVLNKbby0qadXZY0Xt_5Xb9ZLaWSpvpEEw&usqp=CAU"/>
-    //       <img className="book-side" src={bookside} />
-    //     </div>
-    //     {/* </div> */}
-    //     <div className="preface col-md-6">
+    <div className="book-detail container">
+      <div className="heading">New Arrivals</div>
+      <div className="book-container row">
+        {/* <div className="book-coverbg"> */}
+        <div className="col-lg-2 col-md-4 col-sm-6 my-2">
+          <div className="book-cover">
+            <img
+              className="book-top"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGVLNKbby0qadXZY0Xt_5Xb9ZLaWSpvpEEw&usqp=CAU"
+            />
+            <img className="book-side" src={bookside} />
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-4 col-sm-6 my-2">
+          <div className="book-cover">
+            <img
+              className="book-top"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGVLNKbby0qadXZY0Xt_5Xb9ZLaWSpvpEEw&usqp=CAU"
+            />
+            <img className="book-side" src={bookside} />
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-4 col-sm-6 my-2">
+          <div className="book-cover">
+            <img
+              className="book-top"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGVLNKbby0qadXZY0Xt_5Xb9ZLaWSpvpEEw&usqp=CAU"
+            />
+            <img className="book-side" src={bookside} />
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-4 col-sm-6 my-2">
+          <div className="book-cover">
+            <img
+              className="book-top"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGVLNKbby0qadXZY0Xt_5Xb9ZLaWSpvpEEw&usqp=CAU"
+            />
+            <img className="book-side" src={bookside} />
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-4 col-sm-6 my-2">
+          <div className="book-cover">
+            <img
+              className="book-top"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGVLNKbby0qadXZY0Xt_5Xb9ZLaWSpvpEEw&usqp=CAU"
+            />
+            <img className="book-side" src={bookside} />
+          </div>
+        </div>
+        <div className="col-lg-2 col-md-4 col-sm-6 my-2">
+          <div className="book-cover">
+            <img
+              className="book-top"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGVLNKbby0qadXZY0Xt_5Xb9ZLaWSpvpEEw&usqp=CAU"
+            />
+            <img className="book-side" src={bookside} />
+          </div>
+        </div>
+        {/* </div> */}
+        {/* <div className="preface col-md-6">
+          <div className="content">
+            <div className="header">
+              <div className="title">{book.title}</div>
+              <div className="icon"></div>
+            </div>
+            <div className="author">{book.author}</div>
+            <div className="pages">{book.pages}</div>
+            <div className="category">{book.category}</div>
+            <div className="genre">{book.genre}</div>
+            <div className="desc">{book.desc}</div>
+            <div className="price">{book.price}</div>
+          </div>
+        </div> */}
+      </div>
+    </div>
+    //   <div className="book-detail container-fluid">
+    //     <div className="book-container row">
+    //       <div className="book-cover col-md-6">
+    //         1
+    //       </div>
+    //       <div className="preface col-md-6">
     //       <div className="content">
-    //         <div className="header">
-    //           <div className="title">{book.title}</div>
-    //           <div className="icon"></div>
-    //         </div>
-    //         <div className="author">{book.author}</div>
-    //         <div className="pages">{book.pages}</div>
-    //         <div className="category">{book.category}</div>
-    //         <div className="genre">{book.genre}</div>
-    //         <div className="desc">{book.desc}</div>
-    //         <div className="price">{book.price}</div>
+    //            <div className="title">{book.title}</div>
+    //            <div className="author">By - {book.author}</div>
+    //            <div className="pages">{book.pages}</div>
+    //            <div className="category">{book.category}</div>
+    //            <div className="genre">{book.genre}</div>
+    //            <div className="desc">{book.desc}</div>
+    //            <div className="price">{book.price}</div>
+    //          </div>
     //       </div>
     //     </div>
     //   </div>
-    // </div>
-    <div className="book-detail container-fluid">
-      <div className="book-container row">
-        <div className="book-cover col-md-6">
-          1
-        </div>
-        <div className="preface col-md-6">
-        <div className="content">
-             <div className="title">{book.title}</div>
-             <div className="author">By - {book.author}</div>
-             <div className="pages">{book.pages}</div>
-             <div className="category">{book.category}</div>
-             <div className="genre">{book.genre}</div>
-             <div className="desc">{book.desc}</div>
-             <div className="price">{book.price}</div>
-           </div>
-        </div>
-      </div>
-    </div>
   );
 };
 
--- src/Components/UserMain.js
+++ src/Components/UserMain.js
@@ -1,25 +1,79 @@
-import React from 'react'
-
+import React from "react";
+import "../CSS/UserMain.css";
+import buyerbook from "../photos/buyer.jpg";
+import { Link } from "react-router-dom";
+import BookDetail from "./BookDetail";
 const UserMain = () => {
   return (
-    <div className='buyerMain'>
+    <div className="buyerHome">
+      {/* vendor Intro */}
       <div className="container">
-      <div className="buyerHomeIntro row">
+        <div className="buyerHomeIntro row">
           <div className="col-md-7">
-            <h2 className="heading">Sell Books Online</h2>
+            <h2 className="heading">Your Online Book Store</h2>
             <p className="content-intro">
-              If you are a book-seller, here is your chance to reach crores of
-              customers across India by selling on Pretty Pages.
+              Read what you Love.Love what you read.
             </p>
-            <button className="buyerHome-btn">Start selling</button>
+            <button className="buyerHome-btn">Explore Now</button>
           </div>
           <div className="col-md-5 sell-book-img">
-            {/* <img src={sellbook} /> */}
+            <img src={buyerbook} />
+          </div>
+        </div>
+      </div>
+      {/* new arrival */}
+      <BookDetail />
+      {/* services */}
+      <div className="userGenre container-fluid">
+        <h2 className="heading">Popular Genre</h2>
+        <div className="genre row justify-content-center">
+          <div className="col-md-7 col-sm-6">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="box box1">Fantasy</div>
+              </div>
+              <div className="col-md-6">
+                <div className="box box2">Cookbook</div>
+                <div className="box box2">Paranormal</div>
+              </div>
+            </div>
+            <div className="box box3 b4">Motivational</div>
+            <div className="row">
+              <div className="col-md-3">
+                <div className="box box3">Romance</div>
+              </div>
+              <div className="col-md-3">
+                <div className="box box3">Children's</div>
+              </div>
+              <div className="col-md-6">
+                <div className="box box3">Thriller</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="box box3">Health</div>
+              </div>
+              <div className="col-md-6">
+                <div className="box box3">Humor</div>
+              </div>
+            </div>
+            <div className="box box2">Mystery</div>
+            <div className="row">
+              <div className="col-md-6">
+                <div className="box box3">Science</div>
+              </div>
+              <div className="col-md-6">
+                <div className="box box3">History</div>
+              </div>
+            </div>
+            <div className="box box2">Relationships</div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserMain
+export default UserMain;
